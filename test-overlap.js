845d84e14f5f13b799ecc09e02a05c5495ef8d96
Mikola Lysenko <mikolalysenko@gmail.com>
2014-05-01 08:42:23 -0500

    fixed handling denormalized numbers

--- test-overlap.js
+++ test-overlap.js
@@ -5,14 +5,38 @@ module.exports = testOverlap
 var doubleBits = require("double-bits")
 var bits = require("bit-twiddle")
 
-function tz(e, n) {
-  var f = doubleBits.fraction(n)
+function tz(f) {
   if(f[0]) {
-    return e - (52 - bits.countTrailingZeros(f[0]))
+    return bits.countTrailingZeros(f[0])
   } else if(f[1]) {
-    return e - (20 - bits.countTrailingZeros(f[1]))
+    return 32 + bits.countTrailingZeros(f[1])
   } else {
-    return e
+    return 0
+  }
+}
+
+function lz(f) {
+  if(f[1]) {
+    return 20 - bits.log2(f[1])
+  } else if(f[0]) {
+    return 52 - bits.log2(f[0])
+  } else {
+    return 52
+  }
+}
+
+function lo(n) {
+  var e = doubleBits.exponent(n)
+  var f = doubleBits.fraction(n)
+  var z = tz(f)
+  return e - (52 - z)
+}
+
+function hi(n) {
+  if(doubleBits.denormalized(n)) {
+    return -(1023 + lz(doubleBits.fraction(n)))
+  } else {
+    return doubleBits.exponent(n)
   }
 }
 
@@ -25,9 +49,9 @@ function testOverlap(a, b) {
   if(a === 0 || b === 0) {
     return false
   }
-  var ea = doubleBits.exponent(a)
-  var fa = tz(ea, a)
-  var eb = doubleBits.exponent(b)
-  var fb = tz(eb, b)
-  return (fb <= ea) && (fa <= eb)
+  var a0 = hi(a)
+  var a1 = lo(a)
+  var b0 = hi(b)
+  var b1 = lo(b)
+  return (b1 <= a0) && (a1 <= b0)
 }
--- test/test.js
+++ test/test.js
@@ -8,8 +8,16 @@ tape("test-float-overlap", function(t) {
   t.ok(testOverlap(1.5, 0.5))
   t.ok(testOverlap(Math.pow(2, -52), 1.0 + Math.pow(2, -52)))
   t.ok(!testOverlap(1.0, 0.5))
+
+  //Test 0
   t.ok(!testOverlap(0, 1))
   t.ok(!testOverlap(0, 0))
+
+  //Test denormalized numbers
+  t.ok(!testOverlap(Math.pow(2, -1024), Math.pow(2, -1023)))
+  t.ok(!testOverlap(Math.pow(2, -1023), Math.pow(2, -1022)))
+  t.ok(testOverlap(Math.pow(2, -1024) + Math.pow(2, -1040), Math.pow(2, -1030)))
+  t.ok(!testOverlap(Math.pow(2, -1030) - Math.pow(2, -1031), Math.pow(2, -1030)))
 
   t.end()
 })
